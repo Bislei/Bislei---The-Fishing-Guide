855e4a06358c53b8ff54dd24dd9801a3c44172a5
Waseem1437 <waniwaseem1437@gmail.com>
2025-04-24 22:52:02 +0530

    modified splash screen

--- app/src/main/java/com/kashmir/bislei/screens/SplashScreen.kt
+++ app/src/main/java/com/kashmir/bislei/screens/SplashScreen.kt
@@ -1,23 +1,29 @@
 package com.kashmir.bislei.screens
 
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.kashmir.bislei.navigation.Screens
 import kotlinx.coroutines.delay
+import androidx.navigation.NavController
+import com.kashmir.bislei.R
 
 @Composable
-fun SplashScreen(
-    navController: NavHostController,
-    isUserLoggedIn: Boolean
-) {
-    LaunchedEffect(true) {
-        delay(2500)
+fun SplashScreen(navController: NavController, isUserLoggedIn: Boolean) {
 
+    // Optional: Logging navigation decision
+    Log.d("SplashScreen", "User logged in: $isUserLoggedIn")
+
+    LaunchedEffect(Unit) {
+        delay(3000) // Optional: Add splash delay
         if (isUserLoggedIn) {
             navController.navigate(Screens.Home.route) {
                 popUpTo(Screens.Splash.route) { inclusive = true }
@@ -30,18 +36,17 @@ fun SplashScreen(
     }
 
     Box(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background),
         contentAlignment = Alignment.Center
     ) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Spacer(modifier = Modifier.height(20.dp))
-            Column(
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(text = "Bislei", style = MaterialTheme.typography.headlineSmall)
-                Text(text = "The Fishing Guide 🎣", style = MaterialTheme.typography.headlineSmall)
-            }
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Logo", Modifier.size(500.dp))
         }
     }
 }
