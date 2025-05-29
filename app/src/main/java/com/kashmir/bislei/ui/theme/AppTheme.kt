309688f19afdaa201496ed3f5c8c477671ca4f66
Zubair <zubairdin239@gmail.com>
2025-05-29 16:27:01 +0530

    this is new UI Branch with modified Authentication Screens

--- app/src/main/java/com/kashmir/bislei/ui/theme/AppTheme.kt
+++ app/src/main/java/com/kashmir/bislei/ui/theme/AppTheme.kt
@@ -13,8 +13,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-
-
 private val darkColorScheme = AppColorScheme(
     background = bgDark,
     onBackground = Purple80,
